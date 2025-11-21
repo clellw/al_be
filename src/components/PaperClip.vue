457f332012b404c2a4f956d18a7f82fd00d654f5
Clément Beteille <clementbet06@gmail.com>
2025-11-21 16:48:09 +0100

    first scene

---
+++ src/components/PaperClip.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <canvas></canvas>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { FirstScene } from '@/scenes/FirstScene';
+
+export default defineComponent({
+  name: 'PaperClip',
+  mounted(){
+    const canvas = document.querySelector("canvas")!;
+    new FirstScene(canvas);
+  }
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+canvas {
+  width: 70%;
+  height: 70%;
+}
+
+</style>
