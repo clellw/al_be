21594aa36ef5356e1ca7703ccf1f54f39f288810
lalecu <luca.alecu@etu.univ-cotedazur.fr>
2025-12-12 17:36:19 +0100

    aaaaaaaaaaaaaaaa

--- src/scenes/SceneLA.ts
+++ src/scenes/SceneLA.ts
@@ -1,4 +1,4 @@
-import {Scene, Engine, Camera, FreeCamera, Vector3, HemisphericLight, MeshBuilder, SpriteManager, Sprite, FollowCamera, ActionManager, ExecuteCodeAction, StandardMaterial} from "@babylonjs/core"
+import {Scene, Engine, Camera, FreeCamera, Vector3, HemisphericLight, MeshBuilder, SpriteManager, Sprite, FollowCamera, ActionManager, ExecuteCodeAction, StandardMaterial, Mesh} from "@babylonjs/core"
 
 export class SceneLA {
     
@@ -23,7 +23,7 @@ export class SceneLA {
             this.scene
         );
 
-        hemilight.intensity = 0.;
+        hemilight.intensity = 1.0;
 
         const sphere = MeshBuilder.CreateSphere('sphere', {diameter:10, segments:5}, this.scene);
 
@@ -35,7 +35,38 @@ export class SceneLA {
         return scene;
     }
 
+    isCollidingWithBlock (spriteAnchor: Mesh, block: Mesh): boolean{
+        const spriteMin = new Vector3(
+            spriteAnchor.position.x - spriteAnchor.scaling.x * spriteAnchor.getBoundingInfo().boundingBox.extendSize.x,
+            spriteAnchor.position.y - spriteAnchor.scaling.y * spriteAnchor.getBoundingInfo().boundingBox.extendSize.y,
+            0
+        );
+        const spriteMax = new Vector3(
+            spriteAnchor.position.x + spriteAnchor.scaling.x * spriteAnchor.getBoundingInfo().boundingBox.extendSize.x,
+            spriteAnchor.position.y + spriteAnchor.scaling.y * spriteAnchor.getBoundingInfo().boundingBox.extendSize.y,
+            0
+        );
+
+        const blockMin = new Vector3(
+            block.position.x - block.scaling.x * block.getBoundingInfo().boundingBox.extendSize.x,
+            block.position.y - block.scaling.y * block.getBoundingInfo().boundingBox.extendSize.y,
+            0
+        );
+        const blockMax = new Vector3(
+            block.position.x + block.scaling.x * block.getBoundingInfo().boundingBox.extendSize.x,
+            block.position.y + block.scaling.y * block.getBoundingInfo().boundingBox.extendSize.y,
+            0
+        );
+
+        return spriteMin.x <= blockMax.x && spriteMax.x >= blockMin.x &&
+                spriteMin.y <= blockMax.y && spriteMax.y >= blockMin.y;
+    }
+
     async CreateCharacter(scene:Scene): Promise<void> {
+
+        const ground = MeshBuilder.CreateBox('block', {width: 10, height: 0.1, depth: 0.5}, this.scene);
+        ground.position = new Vector3(0,-0.18,0);
+
         //importing the sprites for the character
         const LManager = new SpriteManager(
             'LManager',
@@ -61,13 +92,22 @@ export class SceneLA {
         followCamera.maxCameraSpeed = 100000;
 
         // FollowCamera needs a mesh target, so we create an invisible box to track the sprite
-        const spriteAnchor = MeshBuilder.CreateBox("anchor", {size: 0.1}, scene);
-        spriteAnchor.isVisible = false;
+        const spriteAnchor = MeshBuilder.CreateBox("anchor", {width: lyrina.width/4, height: lyrina.height/1.71, depth:0.1}, scene);
+        spriteAnchor.material = new StandardMaterial('invisibleMat', scene);
+        spriteAnchor.material.wireframe = true;
+        spriteAnchor.isVisible = true;
         spriteAnchor.position = lyrina.position.clone();
+        spriteAnchor.position.y -= 0.009;
         followCamera.lockedTarget = spriteAnchor;
         scene.activeCamera = followCamera;
-
-        
+        let spriteAnchorpotentiel = spriteAnchor.clone("spriteAnchorpotentiel");
+        spriteAnchorpotentiel.isVisible = false;
+        let groundcheat = ground.clone("groundcheat");
+        groundcheat.isVisible = false;
+        while(!this.isCollidingWithBlock(spriteAnchor, ground)){
+            lyrina.position.y -= 0.01;
+            spriteAnchor.position.y -= 0.01;
+        }
         scene.actionManager.registerAction(new ExecuteCodeAction
             (ActionManager.OnKeyDownTrigger,(event)=>{
                 let key = event.sourceEvent.key;
@@ -79,6 +119,7 @@ export class SceneLA {
                 }
             })
         );
+
         scene.actionManager.registerAction(new ExecuteCodeAction
             (ActionManager.OnKeyUpTrigger,(event)=>{
                 let key = event.sourceEvent.key;
@@ -95,6 +136,9 @@ export class SceneLA {
         const speed=0.07;
         let acceleration=0;
         scene.onBeforeRenderObservable.add(()=>{
+            if(!this.isCollidingWithBlock(spriteAnchor, ground)){
+                lyrina.position.y -= 0.001;
+            }
             if(keyStatus.q||keyStatus.s){
                 if(newAnim) {
                     lyrina.playAnimation(9, 13, true, 120);
@@ -102,16 +146,38 @@ export class SceneLA {
                 }
                 if(keyStatus.s && !keyStatus.q){
                     lyrina.invertU = false;
-                    lyrina.position.x += acceleration;
-                    if(acceleration>-speed){
-                        acceleration-=0.004;
+                    spriteAnchorpotentiel = spriteAnchor.clone("spriteAnchorpotentiel");
+                    spriteAnchorpotentiel.isVisible = false;
+                    spriteAnchorpotentiel.position.x += acceleration;
+                    groundcheat = ground.clone("groundcheat");
+                    groundcheat.isVisible = false;
+                    groundcheat.position.y -= 0.01;
+                    if(!this.isCollidingWithBlock(spriteAnchorpotentiel, groundcheat)){
+                        lyrina.position.x += acceleration;
+                        if(acceleration>-speed){
+                            acceleration-=0.004;
+                        }
+                    }
+                    else{
+                        acceleration=0;
                     }
                 }
                 else if(keyStatus.q ){
                     lyrina.invertU = true;
-                    lyrina.position.x += acceleration;
-                    if(acceleration<speed){
-                        acceleration+=0.004;
+                    spriteAnchorpotentiel = spriteAnchor.clone("spriteAnchorpotentiel");
+                    spriteAnchorpotentiel.isVisible = false;
+                    spriteAnchorpotentiel.position.x += acceleration;
+                    groundcheat = ground.clone("groundcheat");
+                    groundcheat.isVisible = false;
+                    groundcheat.position.y -= 0.01;
+                    if(!this.isCollidingWithBlock(spriteAnchorpotentiel, groundcheat)){
+                        lyrina.position.x += acceleration;
+                        if(acceleration<speed){
+                            acceleration+=0.004;
+                        }
+                    }
+                    else{
+                        acceleration=0;
                     }
                 }
             }
@@ -121,11 +187,27 @@ export class SceneLA {
                 }
                 else if(acceleration>0){
                     acceleration-=0.008;
-                    lyrina.position.x += acceleration;
+                    spriteAnchorpotentiel = spriteAnchor.clone("spriteAnchorpotentiel");
+                    spriteAnchorpotentiel.isVisible = false;
+                    spriteAnchorpotentiel.position.x += acceleration;
+                    groundcheat = ground.clone("groundcheat");
+                    groundcheat.isVisible = false;
+                    groundcheat.position.y -= 0.01;
+                    if(!this.isCollidingWithBlock(spriteAnchorpotentiel, groundcheat)){
+                        lyrina.position.x += acceleration;
+                    }
                 }
                 else if(acceleration<0){
                     acceleration+=0.008;
-                    lyrina.position.x += acceleration;
+                    spriteAnchorpotentiel = spriteAnchor.clone("spriteAnchorpotentiel");
+                    spriteAnchorpotentiel.isVisible = false;
+                    spriteAnchorpotentiel.position.x += acceleration;
+                    groundcheat = ground.clone("groundcheat");
+                    groundcheat.isVisible = false;
+                    groundcheat.position.y -= 0.01;
+                    if(!this.isCollidingWithBlock(spriteAnchorpotentiel, groundcheat)){
+                        lyrina.position.x += acceleration;
+                    }
                 }
                 if(acceleration==0){
                     if(!newAnim)lyrina.playAnimation(0,7,true,100);
@@ -134,6 +216,7 @@ export class SceneLA {
             }
             //update the position of the anchor
             spriteAnchor.position.copyFrom(lyrina.position);
+            spriteAnchor.position.y -= 0.009;
             console.log(acceleration);
         });
     }
